8ca56526d8a572a3746d6537533dec946b355bac
Hemant Govekar <hemantgovekar@gmail.com>
2020-09-15 11:18:19 +0530

    files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function getHreyanshBday() {
+    today = new Date();
+    var cmas = new Date(today.getFullYear(), 09, 16);
+
+    if (today.getMonth() == 10 && today.getDate() > 16) {
+        cmas.setFullYear(cmas.getFullYear() + 1);
+    }
+    var one_day = 1000 * 60 * 60 * 24;
+    return Math.ceil((cmas.getTime() - today.getTime()) / one_day) + " days left until Hreyansh bday!";
+}
